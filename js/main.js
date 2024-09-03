dd8ccf4cf9e86c998fce12ec4aa774065d5907f9
eunice-ogboye <ogboyeeunice@gmail.com>
2024-09-03 10:14:14 +0100

    update

--- js/main.js
+++ js/main.js
@@ -82,6 +82,36 @@
         return false;
     });
 
+// Set the date we're counting down to
+var countDownDate = new Date("Sep 14, 2024 00:00:00").getTime();
+
+// Update the countdown every 1 second
+var countdownFunction = setInterval(function() {
+  
+  // Get today's date and time
+  var now = new Date().getTime();
+  
+  // Find the distance between now and the countdown date
+  var distance = countDownDate - now;
+  
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+  // Display the result in the corresponding elements
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+  
+  // If the countdown is over, display a message
+  if (distance < 0) {
+    clearInterval(countdownFunction);
+    document.getElementById("timer").innerHTML = "The big day has arrived!";
+  }
+}, 1000);
 
     // Gallery carousel
     $(".gallery-carousel").owlCarousel({
